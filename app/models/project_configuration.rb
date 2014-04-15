3961d69043ce69f4a15babfab67828734040726e
Ho Minh Hiep <hiephm@smartosc.com>
2014-04-15 11:54:02 +0700

    Allow pure_file for old project

--- app/models/project_configuration.rb
+++ app/models/project_configuration.rb
@@ -13,7 +13,7 @@ class ProjectConfiguration < ConfigurationParameter
 #      'mongrel_rails' => Webistrano::Template::MongrelRails,
 #      'thin_rails' => Webistrano::Template::ThinRails,   
 #      'mod_rails' => Webistrano::Template::ModRails,
-#      'pure_file' => Webistrano::Template::PureFile,
+      'pure_file' => Webistrano::Template::PureFile,
 #      'unicorn' => Webistrano::Template::Unicorn
     }
   end
